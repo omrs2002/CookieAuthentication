7a7c60c77783af198a8f2cb4a65eff84929c2150
Omar Abuhadid <omrs2002@yahoo.com>
2021-07-25 15:01:27 +0300

    Update Users.cs
    Add Date of birth

--- CookieAuthentication.Web/Models/Users.cs
+++ CookieAuthentication.Web/Models/Users.cs
@@ -10,6 +10,7 @@ namespace CookieAuthentication.Web.Models
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Name { get; set; }
+        public DateTime DOB { get; set; }
         public string EmailId { get; set; }
         public string Password { get; set; }
 
